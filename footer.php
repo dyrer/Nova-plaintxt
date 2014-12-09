6da8cbc5bb2326b66d84e398105082c4c6b6dfe3
dyrer <dyrer@hyperwebhost.net>
2014-12-09 16:29:39 -0500

    footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 		<span class="meta-sep">&para;</span>
 		<span id="generator-link"><?php _e('Thanks, <a href="http://wordpress.org/" title="WordPress">WordPress</a>.', 'veryplaintxt') ?></span>
 		<span class="meta-sep">&para;</span>
-		<span id="theme-link"><a href="http://www.plaintxt.org/themes/veryplaintxt/" title="veryplaintxt theme for WordPress" rel="follow designer">veryplaintxt</a> <?php _e('theme by', 'veryplaintxt') ?> <span class="vcard"><a class="url fn n"  title="plaintxt.org" rel="follow designer">PlainTXT.org</a></span>.</span>
+		<span id="theme-link"><a href="http://www.athanasiadis.me/" title="Nova Plaintxt theme for WordPress" rel="follow designer">veryplaintxt</a> <?php _e('theme by', 'Nova veryplaintxt') ?> <span class="vcard"><a class="url fn n"  title="plaintxt.org" rel="follow designer">PlainTXT.org</a></span>.</span>
 		<span class="meta-sep">&para;</span>
 		<span id="web-standards"><?php _e('It\'s nice', 'veryplaintxt') ?> <a href="http://validator.w3.org/check/referer" title="Valid XHTML">XHTML</a> &amp; <a href="http://jigsaw.w3.org/css-validator/validator?profile=css2&amp;warning=2&amp;uri=<?php bloginfo('stylesheet_url'); ?>" title="Valid CSS">CSS</a>.</span>
 	</div><!-- #footer -->
