7e92e6df315db2c50ad4432419bd604a663fb2ce
Evangelos Athanasiadis <dyrer@hyperwebhost.net>
2014-12-19 21:49:56 +0200

    html5

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes() ?>>
 <head profile="http://gmpg.org/xfn/11">
-	<title><?php bloginfo('name') ?><?php if ( is_404() ) : ?> &rsaquo; <?php _e('Page not found', 'veryplaintxt') ?><?php elseif ( is_home() ) : ?> &rsaquo; <?php bloginfo('description') ?><?php elseif ( is_category() ) : ?> &rsaquo; <?php echo single_cat_title(); ?><?php elseif ( is_date() ) : ?> &rsaquo; <?php _e('Blog archives', 'veryplaintxt') ?><?php elseif ( is_search() ) : ?> &rsaquo; <?php _e('Search results', 'veryplaintxt') ?><?php else : ?> &rsaquo; <?php the_title() ?><?php endif ?></title>
+	<title><?php get_bloginfo('name') ?><?php if ( is_404() ) : ?> &rsaquo; <?php _e('Page not found', 'veryplaintxt') ?><?php elseif ( is_home() ) : ?> &rsaquo; <?php get_bloginfo('description') ?><?php elseif ( is_category() ) : ?> &rsaquo; <?php echo single_cat_title(); ?><?php elseif ( is_date() ) : ?> &rsaquo; <?php _e('Blog archives', 'veryplaintxt') ?><?php elseif ( is_search() ) : ?> &rsaquo; <?php _e('Search results', 'veryplaintxt') ?><?php else : ?> &rsaquo; <?php the_title() ?><?php endif ?></title>
 	<meta http-equiv="content-type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />
 	<link rel="stylesheet" type="text/css" media="screen,projection" href="<?php bloginfo('stylesheet_url'); ?>" title="veryplaintxt" />
 	<link rel="stylesheet" type="text/css" media="print" href="<?php bloginfo('template_directory'); ?>/print.css" />
@@ -18,7 +18,8 @@
 <div id="wrapper">
 
 	<div id="header">
-		<h1 id="blog-title"><a href="<?php echo get_option('home') ?>/" title="<?php bloginfo('name') ?>"><?php bloginfo('name') ?></a></h1>
+		<h1 id="blog-title">
+            <h1 id="blog-title"><a href="<?php echo get_option('home') ?>/" title="<?php bloginfo('name') ?>"><?php bloginfo('name') ?></a></h1>
 		<div id="blog-description"><?php bloginfo('description') ?></div>
 	</div><!-- #header -->
 	
